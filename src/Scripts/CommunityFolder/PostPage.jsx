fceadd6b93cbf4b9def49ae824ba3824bef01586
anaungurean <100145653+anaungurean@users.noreply.github.com>
2024-06-06 00:44:17 +0300

    -add-notif

--- src/Scripts/CommunityFolder/PostPage.jsx
+++ src/Scripts/CommunityFolder/PostPage.jsx
@@ -1,40 +1,36 @@
+import { useState, useEffect } from 'react';
+import { useParams } from 'react-router-dom';
 import NavBar from '../../components/SideNavBar';
 import Breadcrumb from '../../components/TopNavBar';
 import PostCard from './components/PostCard';
-import { useState } from 'react';
 import DeletePostPopup from './components/DeletePostPopup';
 import EditPostPopup from './components/EditPostPopup';
 import DeleteCommentPopup from './components/DeleteCommentPopup';
 import Popup from './components/PopUp';
-import { useParams } from 'react-router-dom';
-import { useEffect } from 'react';
- 
 
 function PostPage() {
     const breadcrumbItems = [
-    {
-      name: 'Home',
-      link: '/home',
-    },
-    {
-      name: 'Community Folder',
-      link: '/community',
-    },
-    {
-      name: 'Post',
-      link: '/',
-    },
+        {
+            name: 'Home',
+            link: '/home',
+        },
+        {
+            name: 'Community',
+            link: '/community',
+        },
+        {
+            name: 'Post',
+            link: '/',
+        },
     ];
-    
+
     const [isImageExpanded, setIsImageExpanded] = useState(false);
     const [expandedImageUrl, setExpandedImageUrl] = useState('');
     const [showDeletePopup, setShowDeletePopup] = useState(false);
     const [showEditPopup, setShowEditPopup] = useState(false);
-    const [, setPostId] = useState(null);
-    const [showDeleteCommentPopup, setShowDeleteCommentPopup] = useState(false);
     const [post, setPost] = useState({});
-    const { postId } = useParams();
-    console.log(postId);
+    const [showDeleteCommentPopup, setShowDeleteCommentPopup] = useState(false);
+    const idPost = useParams().idPost;
 
     const toggleImageSize = (imageUrl) => {
         setIsImageExpanded(!isImageExpanded);
@@ -43,12 +39,14 @@ function PostPage() {
 
     useEffect(() => {
         fetchPost();
-    }
-    ,[]);
+        makeSeenPost();
+    }, []);
+
 
     const fetchPost = () => {
+        console.log('Id:', idPost)
         const token = localStorage.getItem('authToken');
-        fetch('http://localhost:5000/questions/' + postId, {
+        fetch(`http://localhost:5000/questions/${idPost}`, {
             method: 'GET',
             headers: {
                 'Content-Type': 'application/json',
@@ -57,9 +55,30 @@ function PostPage() {
         })
         .then(response => response.json())
         .then(data => {
-            console.log('Data:', data); // aici nu se afiseaza nimic in consola
-            setPost(data);
-            
+            console.log('Data:', data);
+            setPost(data[0]);
+            console.log('Post:', post);
+          
+        })
+        .catch((error) => {
+            console.error('Error:', error);
+        });
+    }
+
+    const makeSeenPost = () => {
+        const token = localStorage.getItem('authToken');
+        const idNotification = localStorage.getItem('notification.id');
+        console.log('Idfff:', idNotification)
+        fetch(`http://localhost:5000/seen_notification/${idPost}/${idNotification}`, {
+            method: 'PUT',
+            headers: {
+                'Content-Type': 'application/json',
+                'Authorization': `Bearer ${token}`
+            }
+        })
+        .then(response => response.json())
+        .then(data => {
+            console.log(data);  
           })
         .catch((error) => {
             console.error('Error:', error);
@@ -67,64 +86,49 @@ function PostPage() {
     }
 
 
-
-  return (
-    <div className="flex">
-      <div className="w-1/6">
-        <NavBar />
-      </div>
-      <div className="w-5/6">
-        <Breadcrumb items={breadcrumbItems} />
-        { post && ( 
-        <div className="mt-4 mr-4">
-             <PostCard 
-                            post={post} 
-                            isImageExpanded={isImageExpanded} 
+    return (
+        <div className="flex">
+            <div className="w-1/6">
+                <NavBar />
+            </div>
+            <div className="w-5/6">
+                <Breadcrumb items={breadcrumbItems} />
+                { post.id && (
+                    <div className="mt-4 mr-4">
+                        <PostCard
+                            post={post}
+                            isImageExpanded={isImageExpanded}
                             toggleImageSize={() => toggleImageSize(`http://localhost:5000/questions_photo/${post.id}`)}
                             setShowDeletePopup={() => setShowDeletePopup(true)}
                             setShowEditPopup={() => setShowEditPopup(true)}
-                            setPostId={() => setPostId(postId)}
                             setShowDeleteCommentPopup={() => setShowDeleteCommentPopup(true)}
-                />   
+                        />
 
-             {isImageExpanded && (
-                <Popup imageURL={expandedImageUrl} toggleImageSize={() => toggleImageSize('')} />
-            )}
-            { 
-                        showDeletePopup && (
+                        {isImageExpanded && (
+                            <Popup imageURL={expandedImageUrl} toggleImageSize={() => toggleImageSize('')} />
+                        )}
+                        {showDeletePopup && (
                             <DeletePostPopup
                                 togglePopup={setShowDeletePopup}
-                                postId= {postId}
+                                postId={idPost}
                             />
-                            
-                        )
-                        
-            }
-
-            { 
-                        showEditPopup && (
+                        )}
+                        {showEditPopup && (
                             <EditPostPopup
                                 togglePopup={setShowEditPopup}
-                                post = {postId}
+                                post={idPost}
                             />
-                            
-                        )
-                        
-            }
-
-            {
-                showDeleteCommentPopup && (
-                    <DeleteCommentPopup
-                        togglePopup={setShowDeleteCommentPopup}
-                    />
-                )
-            }   
+                        )}
+                        {showDeleteCommentPopup && (
+                            <DeleteCommentPopup
+                                togglePopup={setShowDeleteCommentPopup}
+                            />
+                        )}
+                    </div>
+                )}
+            </div>
         </div>
-        )}
-
-      </div>
-     </div>
-  );
+    );
 }
 
 export default PostPage;
--- src/components/TopNavBar.jsx
+++ src/components/TopNavBar.jsx
@@ -6,16 +6,14 @@ import { formatDistanceToNowStrict } from 'date-fns'
 
 const Breadcrumb = ({ items }) => {
   const [isMenuOpen, setIsMenuOpen] = useState(false);
-  const [firstName, ] = useState('');
+  const [firstName, ] = useState(localStorage.getItem('firstName'));
   const userId = localStorage.getItem('userId');
   const [notifications, setNotifications] = useState([]);
   const [isNotificationsOpen, setIsNotificationsOpen] = useState(false);
 
   useEffect(() => {
-    // Apelează fetchNotifications la fiecare 5 secunde
-    const interval = setInterval(fetchNotifications, 5000);
-    // Întrerupe intervalul când componenta este demontată
-    return () => clearInterval(interval);
+     const interval = setInterval(fetchNotifications, 5000);
+     return () => clearInterval(interval);
   }, []);
 
   const handleMenuClick = () => {
@@ -59,6 +57,7 @@ const fetchNotifications = () => {
 
   const handleNotificationClick = (notification) => {
     window.location.href = `http://localhost:5173/post/${notification.question_id}`;
+    localStorage.setItem('notification.id', notification.id);
   }
 
   return (
@@ -93,34 +92,34 @@ const fetchNotifications = () => {
     className="rounded-full bg-purple-100 p-2 text-twilight-500 hover:text-twilight-600 focus:outline-none">
     {notifications.length > 0 ? <BellDot size={20} /> : <Bell size={20} />}
   </button>
-  {isNotificationsOpen && (
-    <div className="absolute top-full right-20 bg-purple-50 shadow-md rounded-md w-72 z-100">
-      {notifications.map((notification, index) => (
-      <div key={index} className="flex items-row w-full py-2 px-4 text-sm text-twilight-400 hover:bg-twilight-100/10 focus:outline-none" onClick={() => handleNotificationClick(notification)}>
-          <div className="flex flex-col ">
-           <div className="flex items-center  ">
-            {notification.notification_type === 'like' && <ThumbsUp  size={16} className="mr-2" />}
-            {notification.notification_type === 'comment' && <MessageCircle  size={16} className="mr-2" />}
-            <p className="text-sm font-medium text-twilight-400 mr-2">
-              {notification.notification_type === 'like' && 'New like'}
-              {notification.notification_type === 'comment' && 'New comment'}
-              {notification.notification_type !== 'like' && notification.notification_type !== 'comment' && 'New follow'}
-            </p>
+      {isNotificationsOpen && (
+        <div className="absolute top-full right-20 bg-purple-50 shadow-md rounded-md w-72 z-100">
+          {notifications.map((notification, index) => (
+          <div key={index} className="flex items-row w-full py-2 px-4 text-sm text-twilight-400 hover:bg-twilight-100/10 focus:outline-none" onClick={() => handleNotificationClick(notification)}>
+              <div className="flex flex-col ">
+              <div className="flex items-center  ">
+                {notification.notification_type === 'like' && <ThumbsUp  size={16} className="mr-2" />}
+                {notification.notification_type === 'comment' && <MessageCircle  size={16} className="mr-2" />}
+                <p className="text-sm font-medium text-twilight-400 mr-2">
+                  {notification.notification_type === 'like' && 'New like'}
+                  {notification.notification_type === 'comment' && 'New comment'}
+                  {notification.notification_type !== 'like' && notification.notification_type !== 'comment' && 'New follow'}
+                </p>
+                </div>
+                <p className="text-xs text-gray-400">
+                  {calculateTimeAgo(notification.created_at)}
+                </p>
+                <p className="text-xs text-twilight-400">
+                  {notification.user_name_that_triggered } {notification.notification_type === 'like' && 'liked'} {notification.notification_type === 'comment' && 'commented on'} {notification.notification_type !== 'like' && notification.notification_type !== 'comment' && 'followed'} your post
+                </p>
+              </div>
+              {
+                notification.length > 1 && index < notification.length -1  && <hr className="w-full border-t border-gray-200" />
+              }
             </div>
-            <p className="text-xs text-gray-400">
-              {calculateTimeAgo(notification.created_at)}
-            </p>
-            <p className="text-xs text-twilight-400">
-              {notification.user_name_that_triggered } {notification.notification_type === 'like' && 'liked'} {notification.notification_type === 'comment' && 'commented on'} {notification.notification_type !== 'like' && notification.notification_type !== 'comment' && 'followed'} your post
-            </p>
-          </div>
-           {
-             notification.length > 1 && index < notification.length -1  && <hr className="w-full border-t border-gray-200" />
-           }
+          ))}
         </div>
-      ))}
-    </div>
-  )}
+      )}
 
 </div>
 
